868a28a222a12057b385cc0b7f2f1e1e6711331c
hrkhak <hr.khakii@gmail.com>
2023-02-20 17:25:13 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
+#import librarys and requirements
+##############################################################
+import tensorflow as tf
+from tensorflow.keras import layers
+
+from scipy import ndarray
+
+import skimage as sk
+from skimage import io
+from skimage import util
+from skimage import transform
+
+%matplotlib inline
+
 from datetime import datetime
 import os
 from glob import glob
 from IPython import display
 import imageio
 import matplotlib.pyplot as plt
-%matplotlib inline
 import numpy as np
 import PIL
 from PIL import Image
 import pytz
 tz_NY = pytz.timezone('America/New_York') 
 import random
-from scipy import ndarray
-import skimage as sk
-from skimage import io
-from skimage import util
-from skimage import transform
-import tensorflow as tf
-from tensorflow.keras import layers
 import time
